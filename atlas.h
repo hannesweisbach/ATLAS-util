e7fe04991a9ce6a75bc9f9a2f67f0f22d8b4c8b0
Hannes Weisbach <hannes.weisbach@tu-dresden.de>
2015-05-02 14:55:21 +0200

    Add atlas::submit with std::thread::id / pthread_t
    These versions reside in atlas::np for non-portable since
    1) they rely on hard-coded offset values to get the pid_t from pthread_t and
    2) assume pthread_t is the first element in std::thread::id and can be cast
       to a struct pthread.

--- atlas.h
+++ atlas.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <chrono>
+#include <thread>
+#include <pthread.h>
+#include <algorithm>
+#include <sstream>
+#include <stdexcept>
 
 #include <cerrno>
 
@@ -71,6 +76,58 @@ decltype(auto) submit(pid_t tid, uint64_t id,
   struct timeval tv_deadline = to_timeval(deadline);
 
   return submit(tid, id, &tv_exectime, &tv_deadline, deadline::absolute);
+}
+
+namespace np {
+
+static inline pid_t from(const pthread_t tid) {
+  /* on linux x86_64 glibc has the struct pthread.tid member at (byte-) offset
+   * 720. */
+  const size_t offset = 90;
+  pid_t result;
+  uint64_t *tmp = reinterpret_cast<uint64_t *>(tid) + offset;
+  pid_t *src = reinterpret_cast<pid_t *>(tmp);
+  std::copy(src, src + 1, &result);
+  if (!result) {
+    std::ostringstream os;
+    os << "Thread " << tid << " has either not started or already quit.";
+    throw std::runtime_error(os.str());
+  }
+  return result;
+}
+
+static inline pid_t from(const std::thread::id tid) {
+  /* std::thread::id has a pthread_t as first member in libc++ */
+  return from(*reinterpret_cast<const pthread_t *>(&tid));
+}
+
+template <class Rep1, class Period1, class Rep2, class Period2>
+decltype(auto) submit(const std::thread::id tid, uint64_t id,
+                      std::chrono::duration<Rep1, Period1> exec_time,
+                      std::chrono::duration<Rep2, Period2> deadline) {
+  return atlas::submit(from(tid), id, exec_time, deadline);
+}
+
+template <class Rep1, class Period1, class Rep2, class Period2>
+decltype(auto) submit(const pthread_t tid, uint64_t id,
+                      std::chrono::duration<Rep1, Period1> exec_time,
+                      std::chrono::duration<Rep2, Period2> deadline) {
+  return atlas::submit(from(tid), id, exec_time, deadline);
+}
+
+template <class Rep, class Period, class Clock, class Duration>
+decltype(auto) submit(const std::thread::id tid, uint64_t id,
+                      std::chrono::duration<Rep, Period> exec_time,
+                      std::chrono::time_point<Clock, Duration> deadline) {
+  return atlas::submit(from(tid), id, exec_time, deadline);
+}
+
+template <class Rep, class Period, class Clock, class Duration>
+decltype(auto) submit(const pthread_t tid, uint64_t id,
+                      std::chrono::duration<Rep, Period> exec_time,
+                      std::chrono::time_point<Clock, Duration> deadline) {
+  return atlas::submit(from(tid), id, exec_time, deadline);
+}
 }
 
 static inline decltype(auto) next(void) {
